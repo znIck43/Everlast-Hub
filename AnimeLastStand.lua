502907a74171ea8febc8e1ca5230cc210296fd05
i4go <143762775+znIck43@users.noreply.github.com>
2024-02-13 20:06:29 -0300

    Create AnimeLastStand.lua

---
+++ AnimeLastStand.lua
@@ -0,0 +1,21 @@
+-- wait
+
+_G.codesALS = {
+  "TyFor25mVisitsOMG!", -- 1,500 Gems
+  "200kMembersINSANE!", -- 1.500 Gems
+  "50ThousandsFavorites!!!", -- 750 Gems
+  "UPDATE1HYPE!", -- 500 Gems
+  "Update1TrailerHYPE", -- 500 Gems
+  "BlamsAndShocksNightmare100kMemberReRollCodeTrySubscribingToBlamSpotOnYTAndFollowingFr_ShockOnTwitterIfItDoesntWork", -- 10 Technique Rerolls
+  "BlamsOP5MillionVisitsRerollCodeMustBeSubbedToWorkLOL", -- 5 Technique Rerolls
+  "Sub2HotSauceHan", -- Kohan (Drip) Exotic Unit
+  "ToadBoi120k", -- Gogata (Primal) Exotic Unit
+  "BlamTopSecretCodeWontWorkIfNotSubbed", -- 250 Gems
+  "NeelsTV", -- 250 Gems
+  "D1SGUISED", -- 250 Gems
+  "Sub2KingLuffy", -- 250 Gems
+  "TyFor1mVisitsPart1", -- 1.500 Gems
+  "Sub2BmGTormenter117onYT", -- 250 Gems
+  "RELEASE", -- 750 Gems
+  "SorryForDelay", -- 500 Gems
+}
