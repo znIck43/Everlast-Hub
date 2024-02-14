801f3b6aeafaf7250b25b3e1d42f4c20aa3b1db5
i4go <143762775+znIck43@users.noreply.github.com>
2024-02-13 23:09:57 -0300

    removed comments

--- codes.lua
+++ codes.lua
@@ -115,24 +115,25 @@ _G.codesASTD = {
 }
 
 _G.codesALS = {
-  "TyFor25mVisitsOMG!", -- 1,500 Gems
-  "200kMembersINSANE!", -- 1.500 Gems
-  "50ThousandsFavorites!!!", -- 750 Gems
-  "UPDATE1HYPE!", -- 500 Gems
-  "Update1TrailerHYPE", -- 500 Gems
-  "BlamsAndShocksNightmare100kMemberReRollCodeTrySubscribingToBlamSpotOnYTAndFollowingFr_ShockOnTwitterIfItDoesntWork", -- 10 Technique Rerolls
-  "BlamsOP5MillionVisitsRerollCodeMustBeSubbedToWorkLOL", -- 5 Technique Rerolls
-  "Sub2HotSauceHan", -- Kohan (Drip) Exotic Unit
-  "ToadBoi120k", -- Gogata (Primal) Exotic Unit
-  "BlamTopSecretCodeWontWorkIfNotSubbed", -- 250 Gems
-  "NeelsTV", -- 250 Gems
-  "D1SGUISED", -- 250 Gems
-  "Sub2KingLuffy", -- 250 Gems
-  "TyFor1mVisitsPart1", -- 1.500 Gems
-  "Sub2BmGTormenter117onYT", -- 250 Gems
-  "RELEASE", -- 750 Gems
-  "SorryForDelay", -- 500 Gems
+  "TyFor25mVisitsOMG!",
+  "200kMembersINSANE!",
+  "50ThousandsFavorites!!!",
+  "UPDATE1HYPE!",
+  "Update1TrailerHYPE",
+  "BlamsAndShocksNightmare100kMemberReRollCodeTrySubscribingToBlamSpotOnYTAndFollowingFr_ShockOnTwitterIfItDoesntWork",
+  "BlamsOP5MillionVisitsRerollCodeMustBeSubbedToWorkLOL",
+  "Sub2HotSauceHan",
+  "ToadBoi120k",
+  "BlamTopSecretCodeWontWorkIfNotSubbed",
+  "NeelsTV",
+  "D1SGUISED",
+  "Sub2KingLuffy",
+  "TyFor1mVisitsPart1",
+  "Sub2BmGTormenter117onYT",
+  "RELEASE",
+  "SorryForDelay",
 }
+
 
 _G.codesJAILBREAK = {
   "YoutubeHelloItsVG",
