e933724e473d37b6b119d405b4471003bc6fdb30
i4go <143762775+znIck43@users.noreply.github.com>
2024-02-13 22:51:09 -0300

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,33 @@
+repeat task.wait() until game:IsLoaded()
+
+local Players = game:GetService("Players").LocalPlayer
+local Id = game.PlaceId
+local URLs = {
+    [6299805723] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/AnimeFightersSim.lua",
+    [12886143095] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/AnimeLastStand.lua",
+    [15367026228] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/AnimeSoulsX.lua",
+    [2753915549] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/BloxFruits.lua",
+    [4996049426] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/AllStarTD.lua",
+    [nil] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/BloxFruitsSEA2.lua",
+    [nil] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/BloxFruitsSEA3.lua",
+    [606849621] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/Jailbreak.lua",
+}
+local URL = URLs[Id]
+
+local function LoadScript(name)
+    loadstring(game:HttpGet(name))()
+end
+
+if URL then
+    LoadScript(URL)
+else
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Game not supported",
+        Text = "Copied Discord server link to your clipboard",
+        Icon = "rbxassetid://1234567890"
+    })
+
+    setclipboard("discord.gg/EverlastHub")
+    wait(2.5)
+    Players:Kick("This game is not supported, discord.gg/EverlastHub.")
+end
