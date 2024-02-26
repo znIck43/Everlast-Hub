390fc5e0ffab387f213cfd5bddd3b7b5bdf5c910
i4go <143762775+znIck43@users.noreply.github.com>
2024-02-26 01:33:13 -0300

    Add files via upload

---
+++ Games/AnimeLastStand.lua
@@ -0,0 +1,21 @@
+-- wait
+
+_G.codesALS = {
+    "TyFor25mVisitsOMG!",
+    "200kMembersINSANE!",
+    "50ThousandsFavorites!!!",
+    "UPDATE1HYPE!",
+    "Update1TrailerHYPE",
+    "BlamsAndShocksNightmare100kMemberReRollCodeTrySubscribingToBlamSpotOnYTAndFollowingFr_ShockOnTwitterIfItDoesntWork",
+    "BlamsOP5MillionVisitsRerollCodeMustBeSubbedToWorkLOL",
+    "Sub2HotSauceHan",
+    "ToadBoi120k",
+    "BlamTopSecretCodeWontWorkIfNotSubbed",
+    "NeelsTV",
+    "D1SGUISED",
+    "Sub2KingLuffy",
+    "TyFor1mVisitsPart1",
+    "Sub2BmGTormenter117onYT",
+    "RELEASE",
+    "SorryForDelay",
+  }
