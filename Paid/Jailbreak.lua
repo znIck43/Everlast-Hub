d50de935aa7dea1fbbd1c8118179c052ee1360b7
i4go <143762775+znIck43@users.noreply.github.com>
2024-02-28 11:26:12 -0300

    Add files via upload

---
+++ Paid/Jailbreak.lua
@@ -0,0 +1,7 @@
+-- CODIGO NAO FOI UPADO NO GITHUB AINDA
+local StarterPlayer = game:GetService("StarterPlayer")
+local Ver = require(StarterPlayer.StarterPlayerScripts.Ver)
+
+Ver.modulo_roubo()
+
+-- Main Code
