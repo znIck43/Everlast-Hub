85e92555f3856c0fbd9671080d3041b59a987c1c
i4go <143762775+znIck43@users.noreply.github.com>
2024-02-28 00:36:30 -0300

    Add MakeFolder, setfpscap (krampus.gg), Premium.
    Notify for paid and free version

--- CL/Loader.lua
+++ CL/Loader.lua
@@ -8,22 +8,22 @@ local EH_GUI = game:GetService("CoreGui").CFAHubPremium2022
 local Players = game:GetService("Players").LocalPlayer
 local Id = game.PlaceId
 local Games = {
-    [6299805723] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/AnimeFightersSim.lua",
-    [12886143095] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/AnimeLastStand.lua",
-    [7449423635] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/BloxFruitsSEA3.lua",
-    [4442272183] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/BloxFruitsSEA2.lua",
-    [15367026228] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/AnimeSoulsX.lua",
-    [2753915549] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/BloxFruits.lua",
-    [4996049426] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/AllStarTD.lua",
-    [606849621] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/Jailbreak.lua",
+    [6299805723] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/Games/AnimeFightersSim.lua",
+    [12886143095] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/Games/AnimeLastStand.lua",
+    [7449423635] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/Games/BloxFruitsSEA3.lua",
+    [4442272183] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/Games/BloxFruitsSEA2.lua",
+    [15367026228] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/Games/AnimeSoulsX.lua",
+    [2753915549] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/Games/BloxFruits.lua",
+    [4996049426] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/Games/AllStarTD.lua",
+    [606849621] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/Games/Jailbreak.lua",
 }
 
 -- Functions: Notify, MakeFolder and LoadScript
-local function notify(title, text)
+local function notify(title, text, dur)
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = title,
         Text = text,
-        Icon = "rbxassetid://1234567890"
+        Duration = dur
     })
 end
 
@@ -50,7 +50,9 @@ makefolder("EverlastHub")
 
 if _G.Premium == false and Games[Id] then
     loadscript(Games[Id])
+    notify("Free Script Version", "🟢 = Working", 10)
 elseif _G.Premium == true then
+    notify("Paid Script Version", "🟣 = Paid working", 10)
     loadscript(EH_PS)
 else
     setclipboard("https://discord.gg/gbETDfyS5q")
@@ -60,17 +62,19 @@ end
 -- Check if EH loaded correctly
 if game.Loaded then
     if EH_GUI then
-        notify("Everlast Hub", "🟢")
+        notify("Everlast Hub", "🟢", 7)
     else
         Players:Kick("Everlast Hub did not load correctly. Please rejoin the game.")
     end
 end
 
 -- Anti AFK Mode
-while _G.AntiAFK do
-    notify("Anti AFK = 🟢", "Copied Discord server invite to your clipboard")
+if _G.AntiAFK then
+    notify("Anti AFK = 🟢", "Copied Discord server invite to your clipboard", 7)
     setclipboard("https://discord.gg/gbETDfyS5q")
-    
+
     Players.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
     wait(10)
+    elseif _G.AntiAFK == false then
+        Players.Character.Humanoid:ChangeState(Enum.HumanoidStateType.None)
 end
