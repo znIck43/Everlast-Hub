c6e5b129c318edada21759742f4630721da451d7
1nick <143762775+znIck43@users.noreply.github.com>
2024-04-29 23:36:40 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,37 +1,28 @@
-repeat task.wait() until game:IsLoaded()
+setfpscap(240)
+
+local DiscordInv = "https://discord.gg/gbETDfyS5q"
+setclipboard(DiscordInv)
 
 _G.Premium = false
 
-local DiscordInv = "https://discord.gg/gbETDfyS5q"
-
-local UI = "https://github.com/znIck43/EverlastHub/blob/main/SystemUI.lua"
-local EH_PS = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/EH-PS.lua"
-
 local Player = game:GetService("Players").LocalPlayer
 local Id = game.PlaceId
-local Scripts = {
-    [6299805723] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/Free/AnimeFightersSim.lua",
-    [12886143095] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/Free/AnimeLastStand.lua",
-    [15367026228] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/Free/AnimeSoulsX.lua",
-    [2753915549] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/Free/BloxFruits.lua",
-    [4996049426] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/Free/AllStarTD.lua",
-    [606849621] = "https://raw.githubusercontent.com/znIck43/EverlastHub/main/Free/Jailbreak.lua"
+
+local GamesId = {
+    6299805723,   -- Anime Fighters Simulator
+    12886143095,  -- Anime Last Stand
+    15367026228,  -- Anime Souls Simulator X
+    4996049426,   -- All Star Tower Defense
+    2753915549,   -- Blox Fruits
+    606849621    -- Jailbreak
 }
 
-local function LoadScript(name)
-    loadstring(game:HttpGet(name))()
-end
-
-if Scripts[Id] then
+if table.find(GamesId, Id) then 
     if _G.Premium then
-        LoadScript(EH_PS)
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/znIck43/EverlastHub/main/EH-PS.lua"))()
     else
-        LoadScript(UI)
-        LoadScript(Scripts[Id])
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/znIck43/EverlastHub/main/SystemUI.lua"))()
     end
 else
-    setclipboard(DiscordInv)
-    Player:Kick("Unsupported game, check your clipboard.")
+    Player:Kick("Unsupported game, check your clipboard")
 end
-
-setclipboard(DiscordInv)
