4f72eac7e3ba3168cbc201bb42626123dc18c733
1nick <143762775+znIck43@users.noreply.github.com>
2024-04-29 23:53:01 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -8,16 +8,16 @@ _G.Premium = false
 local Player = game:GetService("Players").LocalPlayer
 local Id = game.PlaceId
 
-local GamesId = {
-    6299805723,    -- Anime Fighters Simulator
-    12886143095,   -- Anime Last Stand
-    15367026228,   -- Anime Souls Simulator X
-    4996049426,    -- All Star Tower Defense
-    2753915549,    -- Blox Fruits
-    606849621      -- Jailbreak
+local Games = {
+    [15367026228] = "Anime Souls Simulator X",
+    [6299805723] = "Anime Fighters Simulator",
+    [4996049426] = "All Star Tower Defense",
+    [12886143095] = "Anime Last Stand",
+    [2753915549] = "Blox Fruits",
+    [606849621] = "Jailbreak"
 }
 
-if table.find(GamesId, Id) then 
+if Games[Id] then
     if _G.Premium then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/znIck43/EverlastHub/main/EH-PS.lua"))()
     else
